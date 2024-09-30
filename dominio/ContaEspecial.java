d6cf41da9736088678499ff81f493cf820a34f01
Aron Barbosa <154107475+aronbarbosag@users.noreply.github.com>
2024-09-30 20:38:55 -0300

    Update ContaEspecial.java

--- dominio/ContaEspecial.java
+++ dominio/ContaEspecial.java
@@ -2,6 +2,10 @@ package fase05.L05Exercicio04.dominio;
 
 	public static class ContaEspecial extends ContaBancaria{
 	    float limite;
+	public ContaEspecial(String cliente, int num_conta, float saldo, float limite) {
+        super(cliente, num_conta, saldo);
+        this.limite = limite;
+    }
 	    public void sacar(float valor){
 	        if(this.saldo>this.limite){
 	            this.saldo-=valor;
