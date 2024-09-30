e4a090c751e60600d860fe2e2eae47f8b172ca19
Aron Barbosa <154107475+aronbarbosag@users.noreply.github.com>
2024-09-30 20:39:26 -0300

    Update ContaPoupanca.java

--- dominio/ContaPoupanca.java
+++ dominio/ContaPoupanca.java
@@ -2,7 +2,10 @@ package fase05.L05Exercicio04.dominio;
 
 public static class ContaPoupanca extends ContaBancaria{
 	    int dia_de_rendimento;
-	    
+	 public ContaPoupanca(String cliente, int num_conta, float saldo, int diaRendimento) {
+        super(cliente, num_conta, saldo);
+        this.diaRendimento = diaRendimento;
+    }
 	    public void calcularNovoSaldo(float taxa){
 	        this.saldo *=(1+(taxa/100));
 	    }
